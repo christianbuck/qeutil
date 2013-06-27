1b0c0f4dac7895ce9a0c71a8e400f37a714d2d14
Christian Buck <cbuck@lantis.de>
2013-06-26 23:15:02 -0700

    print progress

--- wpp/nbest2wpp.py
+++ wpp/nbest2wpp.py
@@ -3,6 +3,7 @@
 import sys
 from math import exp, log1p
 from levenshtein import Levenshtein
+import re
 # 0 ||| también aumentó en México , donde la economía se ha recuperado después de sufrir una caída en la producción del año pasado .  ||| d: 0 -3.89513 0 0 -2.79308 0 0 lm: -75.4448 w: -23 tm: -25.0151 -31.3059 -14.031 -32.8097 9.99896 ||| -9.75692
 
 
@@ -56,12 +57,18 @@ class WPP(object):
         if not buff:
             return res
 
-        first_best_id, first_best = winning_hyp
+        first_best_id, first_best = None, None
+        if winning_hyp == None:
+            first_best_id = buff[0][0]
+            first_best = buff[0][1].split()
+        else:
+            first_best_id, first_best = winning_hyp
         assert first_best_id == curr_id
 
         p_sum = None
         p_sum_exp = 0.0
 
+        # prepare entries in buff - split and align.
         for i in range(len(buff)):
             sent_id, hyp, score = buff[i]
             assert sent_id == curr_id
@@ -100,6 +107,9 @@ if __name__ == "__main__":
     parser.add_argument('-align', action='store_true', help='perform levenshtein alignment')
     parser.add_argument('-firstbest', help='firstbest translations, one per line')
     parser.add_argument('-n', help='limit n-best entries to this many', type=int)
+    parser.add_argument('-skip', help='skip this many lines', type=int)
+    parser.add_argument('-progress', action='store_true', help='more output')
+
 
     args = parser.parse_args(sys.argv[1:])
 
@@ -113,6 +123,7 @@ if __name__ == "__main__":
             for linenr, line in enumerate(open(args.firstbest))]
 
     for linenr, line in enumerate(sys.stdin):
+        line = re.sub(" \|\d+-\d+\| "," ",line )
         line = line.decode('utf-8').strip().split('|||')
         #print repr(line[1])
 
@@ -121,18 +132,33 @@ if __name__ == "__main__":
         line[-1] = float(line[-1])
         line.pop(2)
         # line[1] = line[1].split()
+        if args.skip and args.skip < line[0]:
+            continue
         if curr_id == None or curr_id != line[0]:
             if curr_id != None:
+                # we have either no firstbest at all of the current one better
+                # be in there
                 assert not first_best or curr_id < len(first_best)
                 fb = None if not first_best else first_best[curr_id]
                 if args.n and len(buff) > args.n:
                     buff = buff[:args.n]
                 res = wpp.process_buff(curr_id, buff, fb, align=args.align)
-                print " ".join(map(str, res))
+                sys.stdout.write("%s\n" %(" ".join(map(str, res))))
+                sys.stdout.flush()
+                if args.progress:
+                    sys.stderr.write('.')
+                    sys.stderr.flush()
+                    if (curr_id-1)%50==0:
+                        sys.stderr.write("[%d]\n"%curr_id)
 
             buff = []
             curr_id = line[0]
-        buff.append(list(line))
+        if not args.n or len(buff) < args.n:
+            buff.append(list(line))
 
-    res = wpp.process_buff(curr_id, buff, align=args.align)
-    print " ".join(map(str, res))
+    assert not first_best or curr_id < len(first_best)
+    fb = None if not first_best else first_best[curr_id]
+    if args.n and len(buff) > args.n:
+        buff = buff[:args.n]
+    res = wpp.process_buff(curr_id, buff, fb, align=args.align)
+    sys.stdout.write("%s\n" %(" ".join(map(str, res))))
